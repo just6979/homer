4274d11f508d0cce41945627726ca4028843b7ac
Justin White <just6979@gmail.com>
2011-12-16 17:03:48 -0500

    Removed some old scripts by other people from ~/scripts.

--- scripts/jack_stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pulseaudio --kill
-killall padevchooser
-
--- scripts/stats.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo updating stats for www.justinfiniy.2y.net
-/usr/local/bin/webalizer -Y -p -n justinfinity.2y.net -o /home/www/htdocs/stats /home/www/log/httpd-common.log
-
