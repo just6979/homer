4db6ddb7ca4b638cff047f6ab72666da9b27e866
Justin White <just6979@gmail.com>
2012-10-09 17:26:54 -0400

    Clean up unfinished alias in shortcuts.sh

--- .bash.d/shortcuts.sh
+++ .bash.d/shortcuts.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 #echo -n 'shortcuts, '
-alias setup
-
-
 alias rsync_backup='rsync -r -t -p -o -g -v --progress --delete -u -l -H -i -s -F /home/justin/'
 alias rsync_personal='rsync -r -t -p -o -g -v --progress --delete -u -l -H -i -s --filter="dir-merge .rsync-prsnl-filter" /home/justin/'
 alias jsh='bash -l'
 alias ssh_jj='ssh justin@jester'
 alias ssh_h.jw.net='ssh justin@home.justinwhite.net'
+
 function awsset {
     export AWS=$1;
 }
