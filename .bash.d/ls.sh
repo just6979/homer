71f3fa333e748471ce295b5f9dec4f0ad6607d25
Justin White <just6979@gmail.com>
2013-01-06 01:12:30 -0500

    Added new alias overrides for raspberrypi (GNU ls, no busybox).

--- .bash.d/ls.sh
+++ .bash.d/ls.sh
@@ -2,18 +2,26 @@
 
 # make ls show colors and filetype symbols
 export LSCOLORS='Exfxcxdxbxegedabagacad'
+# Linux uses GNU ls
 if uname -a | egrep -i "linux" &> /dev/null; then
     alias ls='ls -Fh --color=auto';
 fi
+# BSDs don't
 if uname -a | egrep -i "bsd" &> /dev/null; then
     alias ls='ls -Fh -G';
 fi
-if uname -a | egrep -i " arm" &> /dev/null; then
+# Android uses Busybox
+if uname -a | egrep -i " armv7" &> /dev/null; then
     alias ls='busybox ls -Fh --color=auto';
     # also remove some -i prompting, busybox doesn't like them
     unalias rm
     unalias mv
 fi
+# raspberrypi also uses GNU ls
+if uname -a | egrep -i "armv6" &> /dev/null; then
+    alias ls='ls -Fh --color=auto';
+fi
+
 alias lsa='ls -a'
 alias ll='ls -l'
 alias lla='ll -a'
