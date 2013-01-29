1995537aa4539ddab7bb63eb33fad5598f84c40a
Justin White <just6979@gmail.com>
2013-01-29 07:04:39 +0000

    Add user-install'ed ruby gems to the path.

--- .bash.d/env.sh
+++ .bash.d/env.sh
@@ -3,7 +3,7 @@
 #echo -n 'environment, '
 umask 0002 # file perms: 644 -rw-rw-r-- (755 drwxrwxr-x for dirs)
 export EMAIL='just6979@gmail.com'
-export PATH=$PATH:$HOME/scripts:$HOME/Apps:$HOME/bin:$HOME/Apps/depot_tools:$HOME/Android/sdk/tools:$HOME/Android/sdk/platform-tools
+export PATH=$PATH:$HOME/scripts:$HOME/Apps:$HOME/bin:$HOME/Apps/depot_tools:$HOME/Android/sdk/tools:$HOME/Android/sdk/platform-tools:$HOME/.gem/ruby/1.8/bin
 export EDITOR='vim'
 export PAGER='less'
 export LESS='-FMRs~X -x4'
