2e08ffff0c10a3b0923accd596cea9245029f895
Justin White <just6979@gmail.com>
2014-09-30 21:54:55 -0400

    Reorganize pkg tools

--- .bash.d/pkg.sh
+++ .bash.d/pkg.sh
@@ -1,36 +1,42 @@
 #!/usr/bin/env bash
 
 #echo && echo -n 'Package system... '
+
 if [[ -e /etc/debian_version ]]; then
 	#echo 'found Debian base, using apt.'
-	alias service='sudo service'
-#	alias pkg='sudo apt-get'
-	alias pkg='sudo aptitude -V'
+#	alias pkg='apt-get'
+	alias pkg='aptitude -V'
+    alias spkg='sudo pkg'
 	alias pkg2='apt-cache'
 	alias pkginfo='pkg show'
-    alias pkgrm='pkg purge'
 	alias pkglist='pkg pkgnames'
 	alias pkgsearch='pkg search'
-	alias pkgrefresh='pkg update'
-	alias pkgupdate='pkg upgrade -V'
-	alias pkgupgrade='pkg dist-upgrade -V'
-	alias pkgclean='pkg autoremove; pkg autoclean; pkg clean; sudo pkg purge ~c'
-	alias pkgsource='pkg source'
+    alias pkgpurge='spkg purge'
+	alias pkgupdate='spkg update'
+	alias pkgupgrade='spkg upgrade -V'
+	alias pkgupgrade2='spkg dist-upgrade -V'
+	alias pkgclean='spkg autoremove; spkg autoclean; spkg clean; spkg purge ~c'
+	alias pkgsource='spkg source'
 fi
+
 if [[ -e /etc/fedora-release ]]; then
 	#echo 'found Fedora base, using yum.'
-	alias service='sudo service'
-	alias pkg='sudo yum'
+	alias pkg='yum'
+    alias spkg='sudo pkg'
     alias pkginfo='pkg info'
-    alias pkgrm='pkg remove'
     alias pkglist='pkg list'
     alias pkgsearch='pkg search'
-	alias pkgrefresh='pkg makecache'
-	alias pkgupdate='pkg update'
-	alias pkgupgrade='pkg upgrade'
-    alias pkgclean='pkg clean all'
-    alias pkgsource='pkg source'
+	alias pkgupdate='spkg makecache'
+	alias pkgupgrade='spkg update'
+	alias pkgupgrade2='spkg upgrade'
+    alias pkgclean='spkg clean all'
+    alias pkgsource='spkg source'
 fi
+
+alias pkginstall='spkg install'
+alias pkgremove='spkg remove'
+alias pkgcheck='pkgupdate && pkgupgrade'
+
 function pkgsort {
     pkgsearch $1 | sort;
 }
@@ -40,5 +46,8 @@ function pkgless {
 function pkgsearchall {
     pkgsearch all $1 | less;
 }
-alias pkgadd='pkg install'
-alias pkgcheck='pkgrefresh && pkgupdate'
+
+# compat
+alias pkgrm='pkgremove'
+alias pkgadd='pkginstall'
+
